e522c86ef11c8d1404ffbcfb9798d2ec8c2e4202
PACO <mougeot.axel@orange.fr>
2022-11-30 14:14:52 +0100

    add ability actions

--- Assets/Scripts/AbilityCard.cs
+++ Assets/Scripts/AbilityCard.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using Direction;
 
 public class AbilityCard : MonoBehaviour
 {
 
+    public enum action
+    {
+        moveRight,
+        moveLeft,
+        moveUp,
+        moveDown,
+        escalator,
+        teleport,
+        explore
+    }
 
-
-    public bool moveRight, moveLeft, mouveUp, moveDown;
-    public bool escalator, teleport, explore;
+    public action[] actions;
 
     // Start is called before the first frame update
     void Start()
@@ -23,4 +31,41 @@ public class AbilityCard : MonoBehaviour
     {
         
     }
+
+    void DoAction(action a, CharacterController c, Square s = null)
+    {
+        switch(a)
+        {
+            case moveDown:
+                c.Move(Direction.Direction.SOUTH);
+                break;
+            
+            case moveLeft:
+                c.Move(Direction.Direction.WEST);
+                break;
+
+            case moveRight:
+                c.Move(Direction.Direction.EAST);
+                break;
+
+            case moveUp:
+                c.Move(Direction.Direction.NORTH);
+                break;
+
+            case teleport:
+                c.Teleport(s);
+                break;
+            
+            case escalator:
+                c.TakeEscalator();
+                break;
+
+            case explore:
+                c.Explore();
+                break; 
+
+
+        }
+    }
+    
 }
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -32,9 +32,75 @@ public class CharacterController : MonoBehaviour
     {
         switch (d)
         {
+            case Direction.Direction.NORTH:
+                if (currentPosition.up != null)
+                {
+                    currentPosition = currentPosition.up;
+                }
+                else 
+                {
+                    Debug.Log("Error : no square on the top");
+                }
+                break;
+
+            case Direction.Direction.EAST:
+                if (currentPosition.right != null)
+                {
+                    currentPosition = currentPosition.right;
+                }
+                else 
+                {
+                    Debug.Log("Error : no square on the right");
+                }
+                break;
             
+            case Direction.Direction.SOUTH:
+                if (currentPosition.down != null)
+                {
+                    currentPosition = currentPosition.down;
+                }
+                else 
+                {
+                    Debug.Log("Error : no square on the bot");
+                }
+                break;
+
+            case Direction.Direction.WEST:
+                if (currentPosition.left != null)
+                {
+                    currentPosition = currentPosition.left;
+                }
+                else 
+                {
+                    Debug.Log("Error : no square on the left");
+                }
+                break;
+
 
         }
+    }
+
+    void TakeEscalator()
+    {
+        if (currentPosition.escalator != null)
+        {
+            currentPosition = currentPosition.escalator;
+        }
+        else 
+        {
+            Debug.Log("Error : no escalator here");
+        }
+    }
+
+    void Teleport(Square s)
+    {
+        //TODO
+    }
+
+
+    void Explore()
+    {
+        //TODOs
     }
 
 
