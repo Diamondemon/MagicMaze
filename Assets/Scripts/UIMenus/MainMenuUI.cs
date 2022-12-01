da37710f4916ff2765fe1471bebc06103538f41c
Julien Marzal <julien.marzal@telecom-paris.fr>
2022-12-01 17:21:44 +0100

    Created mainmenu scene

--- Assets/Scripts/UIMenus/MainMenuUI.cs
+++ Assets/Scripts/UIMenus/MainMenuUI.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private GameObject mainButtonsPanel;
+    [SerializeField] private GameObject networkManagerPanel;
+    [SerializeField] private GameObject optionsPanel;
+
+    void Awake(){
+        mainButtonsPanel.SetActive(true);
+        networkManagerPanel.SetActive(false);
+        optionsPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void QuitGame(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+
+    public void DisplayNetworkMenu(){
+        mainButtonsPanel.SetActive(false);
+        networkManagerPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    public void DisplayMainMenu(){
+        mainButtonsPanel.SetActive(true);
+        networkManagerPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+
+    public void DisplayOptionsMenu(){
+        mainButtonsPanel.SetActive(false);
+        networkManagerPanel.SetActive(false);
+        optionsPanel.SetActive(true);
     }
 }
--- Assets/Scripts/UIMenus/NetworkManagerUI.cs
+++ Assets/Scripts/UIMenus/NetworkManagerUI.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class NetworkManagerUI : MonoBehaviour
 {
-    private NetworkManager mgr;
     [SerializeField] Button hostButton;
     [SerializeField] Button clientButton;
     [SerializeField] TMP_InputField hostField;
@@ -17,34 +15,26 @@ public class NetworkManagerUI : MonoBehaviour
 
     private void Awake(){
         hostButton.onClick.AddListener(()=>{
-            changeHost();
-            if (NetworkManager.Singleton.StartHost())
-                gameObject.SetActive(false);
+            this.changeHost();
+            if (NetworkManager.Singleton.StartHost()){
+                SceneManager.LoadScene("GameScene");
+            }
         });
         clientButton.onClick.AddListener(()=>{
-            changeHost();
-            if (NetworkManager.Singleton.StartClient())
-                gameObject.SetActive(false);
+            this.changeHost();
+            if (NetworkManager.Singleton.StartClient()){
+                SceneManager.LoadScene("GameScene");
+            }
         });
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 
     // Changes the host adress
     void changeHost(){
-        if (ushort.TryParse(hostField.text, out ushort port)){
+        if (ushort.TryParse(portField.text, out ushort port)){
             NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
                 hostField.text,
                 port
             );
-            NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Port = port;
         }
         else {
             NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
