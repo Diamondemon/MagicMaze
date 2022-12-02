14d14036fdade656c8cd261310e7d7e9ec73173f
Julien Marzal <julien.marzal@telecom-paris.fr>
2022-12-02 11:28:05 +0100

    Options menu minimal

---
+++ Assets/Scripts/UIMenus/OptionsManagerUI.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class OptionsManagerUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeFullScreen(Int32 mode){
+        switch (mode){
+            case 0:
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case 1:
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                break;
+            case 2:
+                Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+                break;
+            case 3:
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                break;
+            default:
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+        }
+    }
+}
