985aeeb20adbb24a862676b6d7ef1652ff505b80
Julien Marzal <julien.marzal@telecom-paris.fr>
2022-12-02 12:33:53 +0100

    Menu for game and network management made better

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class CharacterController : MonoBehaviour
+public class CharacterController : NetworkBehaviour 
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,8 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.L)){
+            transform.position += Vector3.forward * 3* Time.deltaTime;
+        }
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private UIManager uiMgr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        #if UNITY_EDITOR
+        if (!NetworkManager.Singleton.IsHost){
+            NetworkManager.Singleton.StartHost();
+        }
+        #endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)){
+            uiMgr.DisplayEscapeMenu();
+        }
+    }
+}
